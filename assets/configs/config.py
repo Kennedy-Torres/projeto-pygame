4ede9be22b3a7f6ad90b53496916fa838c08d6a6
Kennedy <krtorres.programador@hotmail.com>
2024-06-19 00:36:32 -0300

    docs: arquivo com configs defaults

---
+++ assets/configs/config.py
@@ -0,0 +1,22 @@
+#TAMANHO DA TELA
+LARGURA = 1280
+ALTURA = 720
+
+
+#ARQUIVOS
+BG = 'assets/imgs/bg.png'
+FONTE = 'assets/fonts/PixelGameFont.ttf'
+ALVO ='assets/imgs/target.png'
+MIRA = 'assets/imgs/mouse.png'
+DISPARO = 'assets/audio/disparo.mp3'
+
+#PONTUAÇÃO
+PONTOS = 0
+RECORDE = 0
+
+#TEMPORIZADOR
+TIMER = 600  #1800/60 = 30 SEGUNDOS
+
+#pause & fechar
+GAME_PAUSED = False
+FINALIZAR = False
