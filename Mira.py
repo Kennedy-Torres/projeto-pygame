61ee2ed74c12592f3dd0fa2cc45e7c80fa55f3d2
Kennedy <krtorres.programador@hotmail.com>
2024-06-19 00:40:18 -0300

    fix: corrigindo o contador de ptos

--- Mira.py
+++ Mira.py
@@ -1,23 +1,28 @@
 import pygame
+import random
+from assets.configs.config import MIRA, DISPARO, LARGURA, ALTURA
+from alvo import Alvo
 
 class Mira(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, pontuacao):
         super().__init__()
         self.image = pygame.image.load(MIRA).convert_alpha()
         self.image = pygame.transform.scale(self.image, (50,50))
         self.rect = self.image.get_rect()
         self.sound = pygame.mixer.Sound(DISPARO)
+        self.pontuacao = pontuacao
     
     def update(self):   #predefinido pela Sprite
         self.rect.center = pygame.mouse.get_pos()
     
-    def shoot(self):
-        global PONTOS
+    def shoot(self, grupo_de_alvos):
         self.sound.play()
         
-        colisions = pygame.sprite.spritecollide(mira,grupo_de_alvos, False)
+        colisions = pygame.sprite.spritecollide(self, grupo_de_alvos, False)
         for colision in colisions:
-            PONTOS +=1
+            self.pontuacao.adicionar_ponto()
+            
             colision.kill()
-            alvo = Alvo(random.randrange(0,LARGURA),random.randrange(0,ALTURA)) #só pode desenhar em grupos (conjunto)
-            grupo_de_alvos.add(alvo)
+            while len(grupo_de_alvos) < 6:  # Garante que sempre haja 6 alvos na tela
+                novo_alvo = Alvo(random.randrange(0, LARGURA), random.randrange(0, ALTURA)) #só pode desenhar em grupos (conjunto)
+                grupo_de_alvos.add(novo_alvo)
