442532dd14415843cdff3bc72273c97be572042c
Kennedy <krtorres.programador@hotmail.com>
2024-06-21 19:26:45 -0300

    docs: ...

--- assets/configs/config.py
+++ assets/configs/config.py
@@ -5,17 +5,20 @@ ALTURA = 720
 
 #ARQUIVOS
 BG = 'assets/imgs/bg.png'
+BG_TELA_INICIAL = 'assets/imgs/bg_tela_inicial.png'
 FONTE = 'assets/fonts/PixelGameFont.ttf'
+FONTE_MENU = 'assets/fonts/Mona Shark.otf'
 ALVO ='assets/imgs/target.png'
 MIRA = 'assets/imgs/mouse.png'
 DISPARO = 'assets/audio/disparo.mp3'
+SOUND_MENU = 'assets/audio/sound_menu.mp3'
 
 #PONTUAÇÃO
 PONTOS = 0
 RECORDE = 0
 
-#TEMPORIZADOR
-TIMER = 600  #1800/60 = 30 SEGUNDOS
+#TEMPORIZADOR - baseado em 60fps... 1seg são atualizados 60 quadros
+TIMER = 600  # 600/60 = 10 SEGUNDOS
 
 #pause & fechar
 GAME_PAUSED = False
